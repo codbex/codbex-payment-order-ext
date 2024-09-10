4636c5d031bd5a57985149f04dbc7755e5f55829
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-09-10 10:45:33 +0300

    Add sales order payment (#1)
    * added generating SalesOrderPayment from Customer Payment
    
    * styling changes
    
    * display only unpaid orders
    
    * fix: fix imports and variables
    
    * fix: fix more variables

--- codbex-payment-order-ext/generate/SalesOrderPayment/api/GenerateSalesOrderPaymentService.ts
+++ codbex-payment-order-ext/generate/SalesOrderPayment/api/GenerateSalesOrderPaymentService.ts
@@ -1,4 +1,70 @@
+import { SalesOrderRepository as SalesOrderDao } from "codbex-orders/gen/codbex-orders/dao/SalesOrder/SalesOrderRepository";
+import { CustomerPaymentRepository as CustomerPaymentDao } from "codbex-payments/gen/codbex-payments/dao/CustomerPayment/CustomerPaymentRepository";
+import { SalesOrderPaymentRepository as SalesOrderPaymentDao } from "codbex-orders/gen/codbex-orders/dao/SalesOrder/SalesOrderPaymentRepository";
+
 import { Controller, Get } from "sdk/http";
 
 @Controller
-class GenerateSalesOrderPaymentService { }
+class GenerateSalesOrderPaymentService {
+
+    private readonly salesOrderDao;
+    private readonly customerPaymentDao;
+    private readonly salesOrderPaymentDao
+
+    constructor() {
+        this.salesOrderDao = new SalesOrderDao();
+        this.customerPaymentDao = new CustomerPaymentDao();
+        this.salesOrderPaymentDao = new SalesOrderPaymentDao();
+    }
+
+    @Get("/salesOrderData/:customerPaymentId")
+    public salesOrderData(_: any, ctx: any) {
+        const customerPaymentId = ctx.pathParameters.customerPaymentId;
+
+        const customerPayment = this.customerPaymentDao.findById(customerPaymentId);
+
+        const salesOrder = this.salesOrderDao.findAll({
+            $filter: {
+                equals: {
+                    Customer: customerPayment.Customer
+                }
+            }
+        });
+
+        let notPaidOrders = [];
+
+        salesOrder.forEach((order) => {
+
+            const salesOrderPayments = this.salesOrderPaymentDao.findAll({
+                $filter: {
+                    equals: {
+                        CustomerPayment: customerPayment.Id,
+                        SalesOrder: order.Id,
+                    }
+                }
+            });
+
+            if (salesOrderPayments.length == 0) {
+                notPaidOrders.push(order);
+            }
+
+        });
+
+        return {
+            "salesOrder": notPaidOrders
+        };
+    }
+
+    @Get("/customerPayment/:customerPaymentId")
+    public customerPaymentData(_: any, ctx: any) {
+        const customerPaymentId = ctx.pathParameters.customerPaymentId;
+
+        const customerPayment = this.customerPaymentDao.findById(customerPaymentId);
+
+        return {
+            "Id": customerPayment.Id,
+            "Amount": customerPayment.Amount
+        };
+    }
+
+}
--- codbex-payment-order-ext/generate/SalesOrderPayment/generate-sales-order-payment-controller.js
+++ codbex-payment-order-ext/generate/SalesOrderPayment/generate-sales-order-payment-controller.js
@@ -3,6 +3,49 @@ app.controller('templateController', ['$scope', '$http', 'ViewParameters', 'mess
     const params = ViewParameters.get();
     $scope.showDialog = true;
 
+    const salesOrdersUrl = "/services/ts/codbex-payment-order-ext/generate/SalesOrderPayment/api/GenerateSalesOrderPaymentService.ts/salesOrderData/" + params.id;
+    const customerPaymentUrl = "/services/ts/codbex-payment-order-ext/generate/SalesOrderPayment/api/GenerateSalesOrderPaymentService.ts/customerPayment/" + params.id;
+    const salesOrderPaymentUrl = "/services/ts/codbex-orders/gen/codbex-orders/api/SalesOrder/SalesOrderPaymentService.ts/";
+
+    $http.get(salesOrdersUrl)
+        .then(function (response) {
+            $scope.SalesOrders = response.data.salesOrder;
+        })
+        .catch(function (error) {
+            console.error("Error retrieving data:", error);
+        });
+
+
+    $http.get(customerPaymentUrl)
+        .then(function (response) {
+            $scope.CustomerPayment = response.data;
+        })
+        .catch(function (error) {
+            console.error("Error retrieving data:", error);
+        });
+
+
+    $scope.generateSalesOrderPayment = function () {
+        const salesOrders = $scope.SalesOrders.filter(item => item.selected);
+
+        salesOrders.forEach((salesOrder) => {
+
+            const salesOrderPayment = {
+                "SalesOrder": salesOrder.Id,
+                "CustomerPayment": $scope.CustomerPayment.Id,
+                "Amount": $scope.CustomerPayment.Amount
+            }
+
+            $http.post(salesOrderPaymentUrl, salesOrderPayment)
+                .then(function (response) {
+                    $scope.closeDialog();
+                }).catch(function (error) {
+                    console.error("Error creating Sales Order Payment", error);
+                    $scope.closeDialog();
+                });
+        });
+
+    }
 
     $scope.closeDialog = function () {
         $scope.showDialog = false;
